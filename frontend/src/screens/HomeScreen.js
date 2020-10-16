56c9d7d3bdb01426fe57c3444c1005455a039531
G Sriram <sgcuber24@gmail.com>
2020-10-16 11:49:41 +0530

    Added basic backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import products from '../products';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import axios from 'axios';
+
 const HomeScreen = () => {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get('/api/products');
+			setProducts(data);
+		};
+		fetchProducts();
+	}, []);
 	return (
 		<>
 			<h1>Latest Products</h1>
