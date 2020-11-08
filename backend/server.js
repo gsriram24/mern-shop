200ebf82b5c883ba3cebf49c589975ffa4dcac23
G Sriram <sgcuber24@gmail.com>
2020-11-08 15:32:58 +0530

    Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
-app.get('/', (req, res) => res.send('API is running....'));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -28,6 +27,10 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads/')));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}
 app.use(notFound);
 app.use(errorHandler);
 
